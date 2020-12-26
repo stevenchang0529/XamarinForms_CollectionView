48cba5d6ee167052517538de41c7ed708aec13bc
Steven <steven0529@gmail.com>
2020-12-27 01:12:48 +0800

    加入專案檔案。

---
+++ XamarinForms_CollectionView/XamarinForms_CollectionView/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace _04.MyCollectionView
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            List<string> names = new List<string>() {"Steven","Joy","Niko","Ellen","Kara","Una","Stella","Green","Jerry","Miffy","Cecilia","Karen" };
+            this.collectionView.ItemsSource = names;
+        }
+    }
+}
